c5a73daabe0c708d5f35d0c751e0fe0e6475f882
Zhachory Volker <zhackymoto@gmail.com>
2023-10-26 18:07:44 -0400

    Update heroku_run.sh to default postgre port to 5432

--- deploy/heroku_run.sh
+++ deploy/heroku_run.sh
@@ -7,7 +7,7 @@ export POSTGRE_USER="${BASH_REMATCH[1]}"
 export POSTGRE_PASSWORD="${BASH_REMATCH[2]}"
 export POSTGRE_NAME="${BASH_REMATCH[5]}"
 export POSTGRE_HOST="${BASH_REMATCH[3]}"
-export POSTGRE_PORT="${BASH_REMATCH[4]}"
+export POSTGRE_PORT="5432"
 export DJANGO_DB='default'
 
 ./deploy/docker-entrypoint.sh label-studio \
